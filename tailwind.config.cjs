49abb8a1c1ab7419eac81248f0145f884aab3264
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-06-07 20:18:40 +0700

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,25 +9,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
-    },
-    colors: {
-      emerald: colors.emerald,
-      teal: colors.teal,
-      gray: colors.neutral,
-      ceil: {
-        50: "#F4F5FC",
-        100: "#EAEBF9",
-        200: "#DFE0F5",
-        300: "#D5D6F2",
-        400: "#CACCEF",
-        500: "#BFC2EC",
-        600: "#B5B8E9",
-        700: "#AAADE5",
-        800: "#A0A3E2",
-        900: "#9599DF"
-      }
+      bg_h: 'var(--bg_h)',
+      bg: 'var(--bg)',
+      bg_s: 'var(--bg_s)',
+      bg1: 'var(--bg1)',
+      bg2: 'var(--bg2)',
+      bg3: 'var(--bg3)',
+      bg4: 'var(--bg4)',
+
+      fg: 'var(--fg)',
+      fg1: 'var(--fg1)',
+      fg2: 'var(--fg2)',
+      fg3: 'var(--fg3)',
+      fg4: 'var(--fg4)',
+
+      red: 'var(--red)',
+      green: 'var(--green)',
+      yellow: 'var(--yellow)',
+      blue: 'var(--blue)',
+      purple: 'var(--purple)',
+      aqua: 'var(--aqua)',
+      orange: 'var(--orange)',
+      gray: 'var(--gray)',
+
+      redDim: 'var(--red-dim)',
+      greenDim: 'var(--green-dim)',
+      yellowDim: 'var(--yellow-dim)',
+      blueDim: 'var(--blue-dim)',
+      purpleDim: 'var(--purple-dim)',
+      aquaDim: 'var(--aqua-dim)',
+      orangeDim: 'var(--orange-dim)',
+      grayDim: 'var(--gray-dim)',
     }
-  },
-  plugins: [],
+  }
 }
