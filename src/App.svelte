21447483cdfb6877f6070384b473e6f563d5d335
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-06-07 21:46:40 +0700

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -78,12 +78,14 @@
           Hi there!
           My name is Khanh Hien, but you can call me p3nguin-kun or kh4nhhi3n
           I'm a student, a programmer, a rhythm games player, a tech enthusiast and a FOSS Lover. I've loved computers and technology since I was a kid. I like to participate in FOSS projects that I'm interested in.<br><br>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=PLAwck3ZmnNt2_uPbcSsaTu8qXZ8bAFxYV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </span>
       </div>
       <div class="text-center justify-center invisible xl:visible flex flex-col dark:text-teal-100 text-emerald-900">
         <span>Hi there! <br>
           My name is Khanh Hien, but you can call me p3nguin-kun or kh4nhhi3n <br>
           I'm a student, a programmer, a rhythm games player, a tech enthusiast and a FOSS Lover. I've loved computers and technology since I was a kid. I like to participate in FOSS projects that I'm interested in.<br><br>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=PLAwck3ZmnNt2_uPbcSsaTu8qXZ8bAFxYV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </span>
       </div>
     </div>
