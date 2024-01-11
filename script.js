da8f2063702462120aa5b3fd99e89a96ecb1d321
p3nguin <p3nguinkun@proton.me>
2024-01-11 19:34:49 +0700

    optimize web

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function change_bg() {
-    var totalCount = 3;
+    var totalCount = 4;
     var num = Math.ceil( Math.random() * totalCount );
-    document.body.background = 'assets/rain/'+num+'.gif';
+    document.body.background = 'assets/bg/'+num+'.gif';
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
 }
 
 change_bg();
